f445b947f43ce61465e4e3f29885cba005dcdb70
mustafaozturk34 <mustafa.ozturk3408@gmail.com>
2023-10-19 21:11:07 +0300

    create right bar

--- src/pages/layouts/main/rightbar/index.jsx
+++ src/pages/layouts/main/rightbar/index.jsx
@@ -1,7 +1,13 @@
+import Premium from "~/pages/layouts/main/rightbar/premium";
+import Search from "./search";
+import Topics from "./topics";
+
 export default function RightBar() {
     return (
         <aside className="w-[350px] mr-2.5">
-            Right sidebar
+            <Search />
+            <Premium />
+            <Topics />
         </aside>
     )
 }
